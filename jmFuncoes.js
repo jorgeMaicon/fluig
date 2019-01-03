9ed9417a9dd7cf835d2f8efbed2c8e8829a3810c
Jorge Brigliadori <maikavlis@gmail.com>
2019-01-03 19:02:21 -0200

    Update jmFuncoes.js

--- jmFuncoes.js
+++ jmFuncoes.js
@@ -1,4 +1,4 @@
-//FUNÇÕES 
-function exibirpopUp(){
-  alert("HELLO WORLD");
-}  
+//Dispara mensagem de erro dentro da função beforeSendValidate 
+if(condição){
+     throw "Escreva aqui a mensagem de erro "
+}
