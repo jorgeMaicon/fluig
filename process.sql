5dd3b7c23182cb27c6b9b5539f5721f2d6bfd9f1
Jorge Brigliadori <maikavlis@gmail.com>
2019-01-28 11:39:58 -0200

    Update process.sql

--- process.sql
+++ process.sql
@@ -1,3 +1,31 @@
 --permissão
 select *
 from fluig.ml001667
+
+--permissao-especie
+select *
+from fluig.ml0011711;
+
+--permissao-operacao
+select *
+from fluig.ml0011712;
+
+--obaadm-empresas
+select *
+from fluig.ml0011980
+
+
+
+
+--antecipacao
+select *
+from fluig.ml001619;
+
+--obatarefas
+select *
+from fluig.ml0011764
+--obatarefas-impressora
+select *
+from fluig.ml0011800
+
+
