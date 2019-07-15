9fcea141982a3b27b6b2aafe8ea4bef163a5d8ea
Jorge Brigliadori <maikavlis@gmail.com>
2019-07-15 10:23:25 -0300

    Update fZoom.js

--- fZoom.js
+++ fZoom.js
@@ -1,3 +1,102 @@
+function zoomSetor(){
+	var filtro = $("#unidade").val();
+    tdizoom.open(
+            "get_papeis_aprovadores_novo",
+            "Papel,Setor / Loja,CC,Centro de Custo", "Cod,Desc,Papel,CC",
+            "Selecone o setor/loja responsável",
+            "Cod,"+user+",unidade,"+filtro,
+            "setor"
+            );
+}
+
+function zoomNatureza(){
+	var filtro = $("#unidade").val();
+    tdizoom.open(
+            "dsNaturezaDespesa", //dataset
+            "codhistorico,Codigo Histórico,descricao,Descrição,contadebito,Conta Debito", //campos de exbição chave valor
+            "codhistorico,descricao,contadebito", //campos de retorno
+            "Selecione a natureza",	//label
+            "unidade,"+filtro, //filtro
+            "natureza" //natureza
+            );
+}
+
+function zoomNaturezaCr(){
+	//var filtro = $("#unidade").val();
+    tdizoom.open(
+            "dsnaturezaCr", //dataset
+			"codhistorico,Codigo Histórico,descricao,Descrição,contadebito,Conta Debito", //campos de exbição chave valor
+            "codhistorico,descricao,contadebito", //campos de retorno
+            "Selecione a natureza",	//label
+            "", //filtro
+            "naturezaCr" //natureza
+            );
+}
+
+function getNaturezas(){
+	var equipe  = $("#equipe").val();
+	if (equipe == "cralves"){
+		zoomNaturezaCr();
+	}
+	else{
+		zoomNatureza();
+	}
+}
+
+function zoomSetorGrid(obj){
+	var filtro = $("#unidade").val();
+	var nome = $(obj).prev("input").attr("name"); //Nome do input anterior ao botão para identificar o nome do campo + o ID (rat_setor_nm___1)
+    tdizoom.open(
+            "get_papeis_aprovadores_novo",
+            "Papel,Setor / Loja,CC,Centro de Custo", "Cod,Desc,Papel,CC,Tipo",
+            "Selecone o setor/loja responsável",
+            "Cod,"+user+",unidade,"+filtro,
+            nome
+            );
+}
+
+function zoomContasManutencao(){
+    tdizoom.open(
+            "contas_manutencao",
+            "SEQ,SEQ,desconta,Tipo,status,Descrição", 
+            "SEQ,SEQ,desconta,Descrição da Conta,status,Status",
+            "Selecione uma conta",
+            "",
+            "contasmanutecao"
+            );
+}
+
+function zoomSetorObras(){
+    tdizoom.open(
+            "get_papeis_obras",
+            "Papel,Centro de Custo,CC,.", "Cod,Desc,Papel,CC",
+            "Selecione um centro custo operacional",
+            "Cod,"+user,
+            "setorobra"
+            );
+}
+
+function zoomModelo(){
+    tdizoom.open(
+            "get_modelo_nf",
+            "codmodelo,Código,descricao,Modelo", "codmodelo,Código,descricao,Modelo",
+            "Selecione um Modelo",
+            "",
+            "modelo"
+            );
+}
+
+function zoomEmpresa(){
+	var filtro = $("#unidade").val();
+    tdizoom.open(
+            "get_empresa_nf",
+            "nroempresa,Código,nomereduzido,Empresa", "nroempresa,Código,nomereduzido,Empresa",
+            "Selecione uma Empresa",
+            "unidade,"+filtro,
+            "empresaNf"
+            );
+}
+
 function setSelectedZoomItem(selectedItem) {
     var info = selectedItem.type.split("___");
     
